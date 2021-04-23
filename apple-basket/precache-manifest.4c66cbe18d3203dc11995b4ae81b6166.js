5bbc67ef7f0842100e59d413ed448d278972b6aa
betteryangjie <betteryangjie@qq.com>
2021-04-23 23:35:53 +0800

    苹果篮子

---
+++ apple-basket/precache-manifest.4c66cbe18d3203dc11995b4ae81b6166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86bba1f9f6bc411a18ed5bbcd9dd5927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfaacdc0c8a4c039a680",
+    "url": "/static/css/main.84797af4.chunk.css"
+  },
+  {
+    "revision": "fbfc7b4c4da1cd9392b9",
+    "url": "/static/js/2.8c887984.chunk.js"
+  },
+  {
+    "revision": "b3e2770e6336acd821638b70a2a55e3e",
+    "url": "/static/js/2.8c887984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfaacdc0c8a4c039a680",
+    "url": "/static/js/main.cbab7c50.chunk.js"
+  },
+  {
+    "revision": "dcba97a6314a512bd127",
+    "url": "/static/js/runtime-main.6ed1f6ff.js"
+  }
+]);
