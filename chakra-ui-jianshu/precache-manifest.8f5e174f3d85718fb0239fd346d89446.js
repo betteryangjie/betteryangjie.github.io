0e82cafa4ed58690bb86e5c230b2edd2ef35008e
betteryangjie <betteryangjie@qq.com>
2021-05-04 20:43:08 +0800

    Chakra-UI

---
+++ chakra-ui-jianshu/precache-manifest.8f5e174f3d85718fb0239fd346d89446.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e860437ea8aa5925ae7f12f7172f2e2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7127bca669dff9f3718",
+    "url": "/static/js/2.2ac96ef0.chunk.js"
+  },
+  {
+    "revision": "f925ff808c907f1e05af05a547934faf",
+    "url": "/static/js/2.2ac96ef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe92164bda8d74cd1abe",
+    "url": "/static/js/main.839630aa.chunk.js"
+  },
+  {
+    "revision": "2ad4d5adfc8618d9e86d",
+    "url": "/static/js/runtime-main.92033592.js"
+  }
+]);
